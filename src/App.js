6629302458d9e28c95589cda7e701c45bfa09aa9
ajovicic019 <ajovicic019@gmail.com>
2023-05-31 14:45:57 +0200

    imported bootstrap, added logo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App container">
+      <header className="App-header">
+        <img src={logo} alt="Dictionary logo" className="img-fluid" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
