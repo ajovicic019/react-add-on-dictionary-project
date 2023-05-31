1ceb0b323b77240afed2d82698845fbaeb4e6c48
ajovicic019 <ajovicic019@gmail.com>
2023-05-31 14:49:52 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="Dictionary logo" className="img-fluid" />
       </header>
+      <footer className="text-center">
+        <a
+          href="https://github.com/ajovicic019/react-add-on-dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+          title="Git hub"
+        >
+          Open sourced project
+        </a>{" "}
+        coded by Andrea Jovicic
+      </footer>
     </div>
   );
 }
